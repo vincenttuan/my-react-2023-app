2532761e374c178509266987799c598708625e34
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-07 20:20:05 +0800

    react

---
+++ src/Foo.js
@@ -0,0 +1,8 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Foo() {
+  return "Hello React"
+}
+
+export default Foo;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Foo from './Foo';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Foo />
   </React.StrictMode>
 );
+
+const sendToServer = prefEntity => {
+  fetch('http://localhost:8080/React-backend/pref', {
+    method: 'POST',
+    body: JSON.stringify(prefEntity),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
+reportWebVitals(sendToServer);
