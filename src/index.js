e84a6c82c3cbdd6c59a28c6ac2456bc896de24de
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-09 21:24:25 +0800

    react

--- src/index.js
+++ src/index.js
@@ -4,12 +4,25 @@ import './index.css';
 import App from './App';
 import Foo from './Foo';
 import reportWebVitals from './reportWebVitals';
+// Router
+import AppRouter from './router/AppRouter';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+/*
 root.render(
   <React.StrictMode>
     <App />
     <Foo />
+  </React.StrictMode>
+);
+*/
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
