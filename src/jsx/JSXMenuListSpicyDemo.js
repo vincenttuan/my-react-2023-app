898f13ed70bf089836d162096d090047647985a3
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-14 21:27:10 +0800

    react

---
+++ src/jsx/JSXMenuListSpicyDemo.js
@@ -0,0 +1,25 @@
+import React from "react";
+function JSXMenuListSpicyDemo() {
+    // 這是我們菜單的數據，每個項目有一個id和一個名稱
+    const menuItems = [
+        { id: 1, name: '紅燒牛肉麵', spicy: 2},
+        { id: 2, name: '宮保雞丁', spicy: 2},
+        { id: 3, name: '麻婆豆腐', spicy: 3},
+        { id: 4, name: '蔥油餅', spicy: 0 },
+        { id: 5, name: '炒麵', spicy: 0},
+        { id: 6, name: '炒飯', spicy: 0},
+        { id: 7, name: '炒青菜', spicy: 0},
+        { id: 8, name: '炒時蔬', spicy: 0},
+        { id: 9, name: '炒花枝', spicy: 1},
+        { id: 10, name: '炒蛤蜊', spicy: 1},
+        { id: 11, name: '炒蝦仁', spicy: 0},
+        { id: 12, name: '炒蛋', spicy: 0}
+    ];
+    return (
+        <>
+            
+        </>
+    );
+}
+
+export default JSXMenuListSpicyDemo;
