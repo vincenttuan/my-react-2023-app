99c41ea32635287524c94ae240fe295b8de64c43
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-21 19:36:43 +0800

    react

--- src/form_list/FormAndList.js
+++ src/form_list/FormAndList.js
@@ -12,6 +12,28 @@ class FormAndList extends React.Component {
         }
     }
 
+    // 處理表單欄位變化
+    handleFromChange = (e) => {
+        const {name, value} = e.target;
+        this.setState(prevState => ({
+            form: {...prevState.form, [name]: value}
+        }))
+    };
+
+    // 處理表單提交(表單的資料會放到列表中)
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const newProduct = {
+            id: this.state.products.length - 1,
+            symbol: this.state.form.symbol,
+            amount: this.state.form.amount
+        };
+        this.setState(prevState => ({
+            products: [...prevState.products, newProduct],
+            form: {symbol: '', amount:'0'} // 表單還原預設狀態
+        }))
+    };
+
     render() {
         const {form, products} = this.state;
         return (
@@ -19,6 +41,9 @@ class FormAndList extends React.Component {
                 <FormUI
                     amount={form.amount}
                     symbol={form.symbol}
+                    onSymbolChange={this.handleFromChange}
+                    onAmountChange={this.handleFromChange}
+                    onSubmit={this.handleSubmit}
                 />
                 <ListUI products={products} />
             </div>
