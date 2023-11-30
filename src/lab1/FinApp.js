a54ffabced8e51b52809e82cc5796801493deeef
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-30 21:42:03 +0800

    react

--- src/lab1/FinApp.js
+++ src/lab1/FinApp.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import { StockProvider } from "./StockProvider";
 import StockTradingApp from "./StockTradingApp";
+import ErrorBoundary from "./ErrorBoundry";
 
 function FinApp() {
     return (
-        <StockProvider>
-            <StockTradingApp />
-        </StockProvider>
+        <ErrorBoundary>
+            <StockProvider>
+                <StockTradingApp />
+            </StockProvider>
+        </ErrorBoundary>
     );
 }
 
