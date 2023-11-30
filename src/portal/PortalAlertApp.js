436dfc11dc9e04aaf112963992c3efafa03f1e18
vincenttuan <vincentjava@yahoo.com.tw>
2023-11-30 22:01:36 +0800

    react

---
+++ src/portal/PortalAlertApp.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function AlertBox({onClose}) {
+    return ReactDOM.createPortal(
+        <div className="alert-box">
+            <p>這是一個警告視窗</p>
+            <button onClick="{onClose}">關閉</button>
+        </div>
+    );
+}
+
+function PortalAlertApp() {
+    const [showAlert, setShowAlert] = useState(false);
+    return (
+        <div>
+            <h2>我的 App</h2>
+            <button onClick={() => setShowAlert(true)}>顯示警告視窗</button>
+            {showAlert && <AlertBox onClose={() => setShowAlert(false)} />}
+        </div>
+    );
+}
+
+export default PortalAlertApp;
