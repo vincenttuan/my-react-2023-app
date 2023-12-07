44481646fcd52b97aabeda92771fa7badc2e61e1
vincenttuan <vincentjava@yahoo.com.tw>
2023-12-07 20:28:18 +0800

    react

--- src/lab2/PriceApp.js
+++ src/lab2/PriceApp.js
@@ -9,13 +9,28 @@ function PriceApp() {
     // 當 stockNo 或 date 有變動時才會渲染
     useEffect(() => {
         const url = `/exchangeReport/STOCK_DAY?response=json&date=${date}&stockNo=${stockNo}`;
+        // 透過 fetch API 發送請求
+        fetch(url)
+            .then(response => {
+                // 檢查是否成功 ?
+                if(!response.ok) {
+                    throw new Error("無法取得台灣證券交易所資料");
+                }
+                return response.json();
+            })
+            .then(jsonData => {
+                setJsonData(jsonData);
+            })
+            .catch(error => {
+                console.error('Fetch error:', error);
+            });
 
-    }, [stockNo, date]);
+    }, [stockNo, date]); // 當 stockNo 或 date 有變動時才會渲染
 
     return (
         <div className="center-conatiner" style={{ padding: '15px' }}>
             <h2>歷史股價</h2>
-
+            <pre>{jsonData && console.log(jsonData)}</pre>
         </div>
     )
 }
